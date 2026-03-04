0f7acf445a5464f8c0120b000841298b5502fd82
Shilo <shilo86@gmail.com>
2026-03-03 23:37:41 -0800

    Add index.d.ts type definitions

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+import type { Readable } from "svelte/store";
+
+export type WhisperDictionary = Record<string, unknown>;
+export type WhisperVars = Record<string, unknown> | unknown[];
+
+export function init(options?: {
+    fallback?: string;
+    initial?: string;
+    persistKey?: string;
+    detect?: boolean | Record<string, string>;
+}): Promise<void>;
+
+export function addDictionary(
+    locale: string,
+    dict: WhisperDictionary,
+): void;
+
+export function registerLoader(
+    locale: string,
+    loader: () =>
+        | Promise<WhisperDictionary>
+        | Promise<{ default: WhisperDictionary }>,
+): void;
+
+export function setLocale(locale: string): Promise<void>;
+
+export function getLocales(): string[];
+
+export function getFallbackLocale(): string;
+
+export const locale: {
+    subscribe: Readable<string>["subscribe"];
+    set: (newLocale: string) => Promise<void>;
+};
+
+export const t: Readable<(key: string, vars?: WhisperVars) => string>;
+
+export function tr(
+    key: string,
+    vars?: WhisperVars,
+): string;
